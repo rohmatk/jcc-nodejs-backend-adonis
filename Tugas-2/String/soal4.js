62284aefba379a0b9caf31e16cfba3795be03513
Rohmat Kamil <rohmatkamil450@gmail.com>
2021-08-31 17:38:48 +0700

    Soal 2, 3, 4

---
+++ Tugas-2/String/soal4.js
@@ -0,0 +1,20 @@
+var sentence3 = 'wow JavaScript is so cool'; 
+
+var exampleFirstWord3 = sentence3.substring(0, 3); 
+var secondWord3 = sentence3.substring(4, 14);; // do your own! 
+var thirdWord3 = sentence3.substring(15, 17);; // do your own! 
+var fourthWord3 = sentence3.substring(18, 20); // do your own! 
+var fifthWord3 = sentence3.substring(21, 25); // do your own! 
+
+var firstWordLength = exampleFirstWord3.length  
+var firstSecondWord3 = secondWord3.length
+var firstThirdWord3 = thirdWord3.length
+var firstFouthWord3 = fourthWord3.length
+var firstFifthWord3 = fifthWord3.length
+
+// lanjutkan buat variable lagi di bawah ini 
+console.log('First Word: ' + exampleFirstWord3 + ', with length: ' + firstWordLength); 
+console.log('Second Word: ' + secondWord3 + ', with length: ' + firstSecondWord3); 
+console.log('Third Word: ' + thirdWord3 + ', with length: ' + firstThirdWord3); 
+console.log('Fourth Word: ' + fourthWord3 + ', with length: ' + firstFouthWord3); 
+console.log('Fifth Word: ' + fifthWord3 + ', with length: ' + firstFifthWord3); 
