38d97c10ddc93b4ed11321779ad4ae4e006784f8
Rohmat Kamil <rohmatkamil450@gmail.com>
2021-08-31 22:16:06 +0700

    conditional

---
+++ Tugas-2/Conditional/if-else.js
@@ -0,0 +1,71 @@
+var nama = "Udin";
+var peran = "Penyihir";
+
+if (nama === " " === peran === " ") {
+    console.log('Selamat Bergabung')
+} 
+
+else {
+    console.log('Harus Masukan Nama Disini!')
+} console.log()
+
+if (nama === "John" === peran === " "){
+    console.log('Selamat Bermain')
+} 
+
+else {
+    console.log('Halo John, Pilih Peranmu untuk memulai game!')
+} console.log()
+
+if (nama === "John" === peran ==="Penyihir"){
+    console.log('Harus Bermain')
+} 
+
+else {
+    console.log('Selamat datang di Dunia Werewolf, John')
+}
+
+if (nama !== "John" === peran !== "Werewolf") {
+    console.log('Halo Werewolf John, Kamu akan memakan mangsa setiap malam!')
+} 
+
+else {
+    console.log('Silahkan logout game')
+}
+console.log('Selamat Datang')
+
+if (nama !== "Udin" === peran !== "Penyihir"){
+    console.log('Selamat Datang di Dunia Werewolf, Udin')
+}
+else {
+    console.log('Selamat Datang')
+}
+console.log()
+
+if (nama !== "Udin" === peran !== "Penyihir"){
+    console.log('Halo Penyihir Udin, kamu dapat melihat siapa yang menjadi werewolf!')
+} 
+
+else{
+    console.log('Tidak menemukan Werewolf')
+}
+
+console.log()
+
+if (nama !== "Merlinkundang" === peran !== "Guard"){
+    console.log('Selamat datang di Dunia Warewolf, Merlinkundang')
+} 
+
+else {
+    console.log('Selamat Datang')
+}
+
+console.log()
+
+if (nama !== "merlinkundang" === peran !== "Guard"){
+    console.log('Halo Guard Jenita, kamu akan membantu melindungi temanmu dari serangan werewolf.')
+} 
+
+else{
+    console.log('Merlinkundang adalah guard')
+}
